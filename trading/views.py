1799425194b13d13a4c8e449469d1359bb6a6a37
uzair11 <wajahatahmad056@gmail.com>
2021-03-24 21:27:43 +0500

    displaying account size and brokers

--- trading/views.py
+++ trading/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from trading.models import Broker, AccountSize
+
+
+def make_user(request_obj):
+    mail = request_obj.POST.get('mail')
+    phone = request_obj.POST.get('phone')
+    password = request_obj.POST.get('password')
+    confirmPassword = request_obj.POST.get('confirmPassword')
+    broker = request_obj.POST.get('broker')
+    account = request_obj.POST.get('account')
+
 
 def index(request):
     return render(request, 'trading/index.html')
@@ -12,7 +23,11 @@ def register(request):
     print()
     if request.POST:
         print()
-    return render(request, 'trading/register.html')
+    all_brokers = Broker.objects.all()
+    all_account_size = AccountSize.objects.all()
+
+    context = {'broker': all_brokers, "all_account_size": all_account_size}
+    return render(request, 'trading/register.html', context=context)
 
 
 def contact(request):
